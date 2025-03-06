09d544df2071656aebeb9f886672f1233ffdbaa1
IceK37 <45316097+IceK37@users.noreply.github.com>
2025-03-06 11:15:24 +0100

    errors recettes

--- models/Recette.ts
+++ models/Recette.ts
@@ -5,4 +5,6 @@ export interface Recette {
     cooking_time: number;
     ingredients: { ingredient_id: string; quantity: string }[];
     instructions: string;
+    createdDate: Date;
+    updatedDate: Date;
 }
