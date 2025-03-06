e06947c9ff48b44a1d5b139519715b7d32cd6728
amauryMch <amaurymechin37@gmail.com>
2025-03-06 08:00:10 +0100

    fix

--- routes/routes.ts
+++ routes/routes.ts
@@ -5,6 +5,5 @@ const router = new Router();
 
 router.post("/ingredients", ingredientController.createIngredient);
 router.get("/ingredients", ingredientController.getIngredients);
-router.get("/ingredients/:id", ingredientController.getIngredientById);
 
 export default router;
