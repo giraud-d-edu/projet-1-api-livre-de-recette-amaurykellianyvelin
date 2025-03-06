339932b7fb3b9f2e6ff1c118eeb32492a3e7798e
amauryMch <amaurymechin37@gmail.com>
2025-03-06 11:30:42 +0100

    ✨ IngredientDTO

--- services/IngredientService.ts
+++ services/IngredientService.ts
@@ -1,20 +1,30 @@
 import * as ingredientRepository from "../repositories/IngredientRepository.ts";
 import { Ingredient } from "../models/Ingredient.ts";
+import { convertToIngredientNameDto } from "../controllers/dtos/IngredientDTO.ts"
 
 export const createIngredient = (ingredient: Ingredient) => {
     const ingredientWithDate = {
         ...ingredient,
         createdDate: new Date(),
     };
-    return ingredientRepository.createIngredient(ingredientWithDate);
+    return ingredientRepository.createIngredient(ingredientWithDate)
+        .then((ingredient) => {
+            return convertToIngredientNameDto(ingredient);
+        });
 }
 
 export const getIngredients = () => {
-    return ingredientRepository.getIngredients();
+    return ingredientRepository.getIngredients()
+        .then((ingredients) => {
+            return ingredients.map((ingredient) => convertToIngredientNameDto(ingredient));
+        });
 }
 
 export const getIngredientById = (id: string) => {
-    return ingredientRepository.getIngredientById(id);
+    return ingredientRepository.getIngredientById(id)
+        .then((ingredient) => {
+            return convertToIngredientNameDto(ingredient);
+        });
 }
 
 export const updateIngredient = (id: string, ingredient: Ingredient) => {
