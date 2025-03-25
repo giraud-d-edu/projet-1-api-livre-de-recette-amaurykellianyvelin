c01a15b1cfb83400e545c43330ba470ad1316cda
IceK37 <45316097+IceK37@users.noreply.github.com>
2025-03-25 13:23:57 +0100

    ester eggs

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
 import { Application } from "./deps.ts";
 import router from "./routes/routes.ts";
 import { errorHandler } from "./middlewares/ErrorHandler.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const app = new Application();
 
 app.use(errorHandler);
+
+app.use(oakCors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+}));
 
 app.use(router.routes());
 app.use(router.allowedMethods());
